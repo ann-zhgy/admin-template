10bc3940f3b2c16a426980f9b4b7408c5004b7b7
张高瑜 <zhanggaoyu@workatdata.com>
2024-12-05 14:21:10 +0800

    feat: simple 更新sql和初始化数据

--- admin-template-with-simple/src/main/resources/init-sql.sql
+++ admin-template-with-simple/src/main/resources/init-sql.sql
@@ -15,4 +15,12 @@ CREATE TABLE `user`
     UNIQUE KEY `uni_email` (`email`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_0900_ai_ci COMMENT ='账号信息';
+  COLLATE = utf8mb4_general_ci COMMENT ='账号信息';
+
+-- password：admin
+INSERT INTO `admin-template-simple`.user (username, password, phone, email, permission, status, create_time, update_time) VALUES ('admin', '$2a$10$LmhRKSZDzOVm6bDm097/Ru9MiKmf7CgP/vDZ4U5vvjdATNfhTXPIC', null, null, 131, 1, '2024-12-02 03:47:09', '2024-12-04 03:12:08');
+-- password：test
+INSERT INTO `admin-template-simple`.user (username, password, phone, email, permission, status, create_time, update_time) VALUES ('test', '$2a$10$chBPolPjw3e/1dhTGW3t..Xra4z1YRL3Kwa6edlwai093iBBSpH5a', '', '', 3, 1, '2024-12-03 07:21:27', '2024-12-05 06:13:50');
+-- password：test2
+INSERT INTO `admin-template-simple`.user (username, password, phone, email, permission, status, create_time, update_time) VALUES ('test2', '$2a$10$nt6OUDD/bkXYvPeHVQ2CP.iGxSnQFpslxkO7VzlbJKQEtmyrv7tHu', null, null, 1, 2, '2024-12-04 02:37:15', '2024-12-05 06:00:47');
+
